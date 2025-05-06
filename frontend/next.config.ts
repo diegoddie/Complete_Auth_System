a4ddf5cad1fd2accd9e7d56e7840d3d155e767ee
Diego <diego.boost@gmail.com>
2025-05-06 19:42:26 +0200

    minor

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const API_BASE_URL = process.env.API_BASE_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL}/api/:path*`,
+        destination: `${API_BASE_URL}/api/:path*`,
       },
     ];
   },
